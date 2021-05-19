76889ff12ee59d8f1becae5355953b772de2dd14
kwc1907 <1907137461@qq.com>
2021-05-19 15:55:19 +0800

    冒泡排序

---
+++ 冒泡排序/冒泡排序.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a[5],i,j,t;
+	printf("ÇëÊäÈëÊı×Ö£º");
+	for(i=0;i<5;i++)
+	scanf("%d",&a[i]);
+	for(j=0;j<4;j++)
+	for(i=0;i<4-j;i++)
+	if(a[i]>=a[i+1])
+	{
+	t=a[i];a[i]=a[i+1];a[i+1]=t;
+	}
+	for(i=0;i<5;i++)
+	printf("%5d",a[i]);
+	 
+}
